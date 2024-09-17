60b9511481fe9278de09d60f856eaf3219c3b71d
Tiago9851 <Tiago98k5146@gmail.com>
2024-09-17 00:05:04 +0000

    atualizando código

---
+++ tiposPrimitivos.js
@@ -0,0 +1,7 @@
+const rotulo="natalya" // declaramos a variavel
+rotulo=12345 // retribuimos o tipo de dado
+console.log(rotulo);
+
+var name="jonas"
+var idade="22"
+ola meu nome é (name) e tenho (idade) anos 
